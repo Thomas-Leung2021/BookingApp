ff73c2997c49bcf5c9bca2f1d65b8ab7b31354cd
Thomas Leung <thomas.leung@netexperience.com>
2022-07-08 18:17:18 -0400

    add user verification

--- api/README.md
+++ api/README.md
@@ -1,2 +1,5 @@
 npm init -y
 npm i express nodemon dotenv mongoose
+
+https://www.youtube.com/watch?v=k3Vfj-e1Ma4
+1:11:00
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,5 +1,36 @@
-import express from "express";
+import express from 'express';
+import {
+  deleteUser,
+  getUser,
+  getUsers,
+  updateUser,
+} from '../controllers/user.js';
+import { verifyAdmin, verifyToken, verifyUser } from '../utils/verifyToken.js';
 
 const router = express.Router();
 
-export default router
+router.get('/checkauthentication', verifyToken, (req, res, next) => {
+  res.send('Hello, you are logged in');
+});
+
+router.get('/checkuser/:id', verifyUser, (req, res, next) => {
+  res.send('Hello, you are logged in and you can delete your account.');
+});
+
+router.get('/checkadmin/:id', verifyAdmin, (req, res, next) => {
+  res.send('Hello admin, you are logged in and you can delete accounts.');
+});
+
+// UPDATE
+router.put('/:id', updateUser);
+
+// DELETE
+router.delete('/:id', deleteUser);
+
+// GET
+router.get('/:id', getUser);
+
+// GET ALL
+router.get('/', getUsers);
+
+export default router;
